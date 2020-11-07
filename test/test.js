d39eed5a4223398e565eaeaf5e9154d0d0920934
Chechuck <chechu.sparrow@gmail.com>
2020-11-07 22:55:42 +0100

    [production] another test

--- test/test.js
+++ test/test.js
@@ -11,4 +11,9 @@ describe('Add', function () {
     const result = add(1, 2);
     assert.equal(result, 3);
   });
+
+  it('Should return 4', function () {
+    const result = add(2, 2);
+    assert.notEqual(result, 2);
+  });
 });
